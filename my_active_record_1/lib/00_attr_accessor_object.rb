396fd10499a7b1e91d6bcd60be7036756085f8ce
ericswetts <eric.swetts@gmail.com>
2018-11-09 11:54:53 -0800

    Complete attr-accessor_object exercise

--- my_active_record_1/lib/00_attr_accessor_object.rb
+++ my_active_record_1/lib/00_attr_accessor_object.rb
@@ -1,5 +1,27 @@
 class AttrAccessorObject
   def self.my_attr_accessor(*names)
-    # ...
+    names.each do |name|
+      define_method(name) do
+        p "defining a getter"
+        instance_variable_get("@#{name}")
+      end
+
+      define_method("#{name}=") do |value|
+        p "defining a setter"
+        instance_variable_set("@#{name}", value)
+      end
+
+
   end
 end
+end
+
+
+# def self.my_attr_accessor=(*names, value)
+#   names.each do |name|
+#     p "defining_a_setter"
+#     define_method(name) do
+#       instance_variable_set(name)
+#     end
+#
+# end
